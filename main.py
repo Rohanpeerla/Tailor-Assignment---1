2018c8114a1feea2b8b43242fb605804528b69f1
Rohanpeerla <144145257+Rohanpeerla@users.noreply.github.com>
2026-02-26 18:38:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from agent import run_query
+
+app = FastAPI()
+
+class Query(BaseModel):
+    question: str
+
+@app.get("/")
+def root():
+    return {"message": "Titanic backend is running", "health": "/health", "docs": "/docs"}
+
+@app.post("/ask")
+def ask(query: Query):
+    answer, image = run_query(query.question)
+    return {
+        "answer": answer,
+        "image": image
+    }
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
